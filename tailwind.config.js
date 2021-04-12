8043b4e2199c08bef36c9bf7151989d49599afeb
Angel Ramirez <pepe_199904@hotmail.com>
2021-04-11 23:29:18 -0500

    Dandole Formato al Logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
         extend: {
             backgroundImage: theme => ({
                 'body-pattern': "url('../img/pattern.png')"
-            })
+            }),
+            colors: {
+                "azul-claro": "#37bcf9",
+                "azul-obscuro": "#0370b9",
+                "obscuro-raxe": "#020215",
+                "azul-raxe": "#6196e4"
+            }
 
         }
     },
